c44ffec2eac9592e9b1039d0350cf1903e4c92b1
Christopher Bertels <bakkdoor@flasht.de>
2013-08-15 15:43:19 +0200

    add find_links_new example which uses the D wrapper API

---
+++ examples/find_links_new.d
@@ -0,0 +1,56 @@
+import gumbo.node, gumbo.capi, gumbo.parse;
+
+import std.stdio, std.file;
+import std.string : toStringz;
+import std.conv : text;
+
+string[] find_links(gumbo.node.Node node) {
+    string[] links;
+
+    if (node.type != GumboNodeType.GUMBO_NODE_ELEMENT) {
+        return links;
+    }
+
+    gumbo.node.Element element = cast(gumbo.node.Element) node;
+
+    if (element.tag == GumboTag.GUMBO_TAG_A) {
+        GumboAttribute* href = element.getAttribute("href");
+        if(href) {
+            links ~= text(href.value);
+        }
+    }
+
+    Node[] children = element.children;
+    foreach(Node child; children) {
+        links ~= find_links(child);
+    }
+
+    return links;
+}
+
+void usage() {
+    stderr.writeln("Usage: find_links <html filename>");
+}
+
+int main(string[] argv) {
+    if (argv.length != 2) {
+        usage();
+        return 1;
+    }
+
+    string filename = argv[1];
+
+    scope(failure) {
+        stderr.writeln("Error: ", filename, " is not a file");
+        usage();
+        return 1;
+    }
+
+    gumbo.parse.Output output = gumbo.parse.parseFile(filename);
+    foreach(link; find_links(output.root)) {
+        writeln(link);
+    }
+    output.destroy(&kGumboDefaultOptions);
+
+    return 0;
+}
