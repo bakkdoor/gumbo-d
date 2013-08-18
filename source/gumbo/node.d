8e8c88180403a469b6dff0f349f29546397c8e58
Christopher Bertels <bakkdoor@flasht.de>
2013-08-18 19:47:03 +0200

    add Node#mapChildren and Node#flatMapChildren template methods

--- source/gumbo/node.d
+++ source/gumbo/node.d
@@ -77,6 +77,23 @@ public:
         return null;
     }
 
+    T[] mapChildren(T)(T function(Node) fun)
+    {
+        T[] vals;
+        foreach(child; children) {
+            vals ~= fun(child);
+        }
+        return vals;
+    }
+
+    T[] flatMapChildren(T)(T[] function(Node) fun)
+    {
+        T[] vals;
+        foreach(child; children) {
+            vals ~= fun(child);
+        }
+        return vals;
+    }
 
     static Node fromCAPI(GumboNode * node)
     {
