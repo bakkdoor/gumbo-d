7107300fb8116cab060ed86c1b9b2641d46789ed
Christopher Bertels <bakkdoor@flasht.de>
2013-08-18 19:27:31 +0200

    refactored get_title example using new Node#findChild template method

--- examples/get_title.d
+++ examples/get_title.d
@@ -1,44 +1,17 @@
-import gumbo.node, gumbo.parse, gumbo.capi;
+import gumbo.node, gumbo.parse;
 import std.stdio;
 
 string findTitle(Node root)
 {
-    assert(root.type == Node.Type.ELEMENT);
-    return findTitle(cast(Element)root);
-}
+    Element titleElem = root.findChild!(Element)(e => e.tag == Element.Tag.TITLE);
+    if(!titleElem)
+        return "<no title found>";
 
-string findTitle(Element element)
-{
-    assert(element.children.length >= 2);
-
-    Node head = null;
-
-    foreach(child; element.children) {
-        if(child.type == Node.Type.ELEMENT &&
-            (cast(Element)child).tag == Element.Tag.HEAD) {
-            head = child;
-            break;
-        }
-    }
-
-    assert(head !is null);
-
-    foreach(child; (cast(Element)head).children) {
-        if(child.type == Node.Type.ELEMENT) {
-            Element childElement = cast(Element) child;
-            if(childElement.tag == Element.Tag.TITLE) {
-                if(childElement.children.length != 1) {
-                    return "<empty title>";
-                }
-
-                Node titleText = childElement.children[0];
-                assert(titleText.type == Node.Type.TEXT);
-                return (cast(Text)titleText).text;
-            }
-        }
-    }
-
-    return "<no title found>";
+    Text title = titleElem.findChild!(Text)(e => e.type == Node.Type.TEXT);
+    if(title)
+        return title.text;
+    else
+        return "<empty title>";
 }
 
 void usage()
@@ -62,8 +35,7 @@ int main(string[] argv)
     }
 
     Output output = gumbo.parse.Output.fromFile(filename);
-    string title = findTitle(output.root);
-    writeln(title);
+    writeln(findTitle(output.root));
     output.destroy();
 
     return 0;
